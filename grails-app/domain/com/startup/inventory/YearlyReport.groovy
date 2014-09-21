f3edce4d740c6768c75bb9326d0c0de6ff02376c
Rumee <rumee.cse@gmail.com>
2014-09-21 21:56:34 +0600

    Jasper

--- grails-app/domain/com/startup/inventory/YearlyReport.groovy
+++ grails-app/domain/com/startup/inventory/YearlyReport.groovy
@@ -4,11 +4,13 @@ class YearlyReport {
     CategoryType categoryType
     ProductItem productItem
     String amount
+    String month
 
     static constraints = {
         categoryType nullable: true, blank: true
         productItem nullable: true, blank: true
         amount nullable: true, blank: true
+        month nullable: true, blank: true
     }
 
 }
